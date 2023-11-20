80da8d86c04fc8589932402bb10b7781a7eacfd4
Jeroen Ooms <jeroenooms@gmail.com>
2023-11-20 13:32:13 +0100

    add some more packages to test

--- tests/download.R
+++ tests/download.R
@@ -2,7 +2,8 @@
 pkgs <- c('ragg', 'stringi', 'igraph', 'V8', 'opencv', 'magick', 'pdftools', 
   'protolite', 'curl', 'openssl', 'RMariaDB', "RPostgres", "glmnet", "av",
   "gert", 'mongolite', 'rsvg', 'rzmq', 'sodium', 'tesseract', 'writexl',
-  "shiny", "tidyverse", 'RcppParallel')
+  "shiny", "tidyverse", 'RcppParallel', 'antiword', 'unrtf', 'brotli',
+  'cld2', 'gert', 'hunspell', 'RMySQL', 'rzmq', 'ssh', 'webp', 'writxl')
 
 # Install host binaries + dependencies
 install.packages(pkgs, repos = 'https://p3m.dev/cran/__linux__/jammy/latest')
