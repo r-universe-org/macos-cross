654bdeaf250205aa5e230bea2a87d0e6329d766c
Jeroen Ooms <jeroenooms@gmail.com>
2023-11-20 14:17:35 +0100

    more test cases

--- tests/download.R
+++ tests/download.R
@@ -3,7 +3,8 @@ pkgs <- c('ragg', 'stringi', 'igraph', 'V8', 'opencv', 'magick', 'pdftools',
   'protolite', 'curl', 'openssl', 'RMariaDB', "RPostgres", "glmnet", "av",
   "gert", 'mongolite', 'rsvg', 'rzmq', 'sodium', 'tesseract', 'writexl',
   "shiny", "tidyverse", 'RcppParallel', 'antiword', 'unrtf', 'brotli',
-  'cld2', 'gert', 'hunspell', 'RMySQL', 'rzmq', 'ssh', 'webp', 'writxl')
+  'cld2', 'gert', 'hunspell', 'RMySQL', 'rzmq', 'ssh', 'webp', 'writxl',
+  'glmnet', 'sparsenet', 'gam', 'data.table', 'sf', 'stars')
 
 # Install host binaries + dependencies
 install.packages(pkgs, repos = 'https://p3m.dev/cran/__linux__/jammy/latest')
